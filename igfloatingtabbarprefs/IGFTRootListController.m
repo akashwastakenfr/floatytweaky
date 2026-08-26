ea08a5ba3b9ec445ed0201f543620d0a24872675
akash <akashwastakenfr@gmail.com>
2026-08-26 13:21:06 +0600

    Add UIKit and Preferences imports to IGFTRootListController

--- igfloatingtabbarprefs/IGFTRootListController.m
+++ igfloatingtabbarprefs/IGFTRootListController.m
@@ -1,5 +1,7 @@
+#import <UIKit/UIKit.h>
 #import <Preferences/PSListController.h>
 #import <Preferences/PSSpecifier.h>
+#import <Preferences/PSHeaderFooterView.h>
 #import <spawn.h>
 
 extern char **environ;
